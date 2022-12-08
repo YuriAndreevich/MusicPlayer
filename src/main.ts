6d3e0e8b38b1a9a6a54bc1d5671c2d9591a42a16
YURII SILIN <try2scumme@gmail.com>
2022-12-09 00:07:50 +0300

    1

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 3001;
+    const app = await NestFactory.create(AppModule);
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
