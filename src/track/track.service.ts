bd9c537ce9eb405e518a5ee3adde991288c222ba
AlexSavr <YuriAndreev11ch@gmail.com>
2022-12-16 13:25:37 +0300

    1

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Track } from './schemas/track.schema';
 import { Comment } from './schemas/comment.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import  {TrackDocument} from './track.module'
 
 @Injectable()
 export class TrackService {
   constructor(
-    @Injectable(Track.name) private trackModel: Model<TrackDocument>,
-    @Injectable(Comment.name) private commentkModel: Model<CommentDocument>,
+    @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+    @InjectModel(Comment.name) private commentkModel: Model<CommentDocument>,
   ) {}
 
-  async create() {}
+  async create(dto: CreateTrackDto): Promise<Track> {
+    const track = await this.trackModel.create({...dto. listens: 0})
+  return track;
+  }
   async getAll() {}
 
   async getOne() {}
